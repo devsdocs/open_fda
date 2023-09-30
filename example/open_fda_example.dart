da4651b1fe45683f7a48adc06f6f330ed6c4d5b0
devsdocs <73208025+devsdocs@users.noreply.github.com>
2023-10-01 06:22:48 +0900

    WIP

---
+++ example/open_fda_example.dart
@@ -0,0 +1,6 @@
+// import 'package:open_fda/open_fda.dart';
+
+// void main() {
+//   var awesome = Awesome();
+//   print('awesome: ${awesome.isAwesome}');
+// }
---
+++ test/open_fda_test.dart
@@ -0,0 +1,16 @@
+// import 'package:open_fda/open_fda.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   group('A group of tests', () {
+//     final awesome = Awesome();
+
+//     setUp(() {
+//       // Additional setup goes here.
+//     });
+
+//     test('First Test', () {
+//       expect(awesome.isAwesome, isTrue);
+//     });
+//   });
+// }
